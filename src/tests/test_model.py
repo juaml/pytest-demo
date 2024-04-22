68663733b3968845ca2d00b479f854ce732468bc
LeSasse <l.sasse@fz-juelich.de>
2024-04-22 16:52:33 +0200

    fix imports for mutpy

--- src/tests/test_model.py
+++ src/tests/test_model.py
@@ -1,5 +1,19 @@
-from ..model import add, mul, predict
 
+
+import sys
+from pathlib import Path
+
+# We import is a bit messier and could simply
+# be:
+# from ..model import add, mul, predict
+# if we only wanted to use pytest.
+# The below import enables us to use mut.py
+# as well as pytest:
+src = Path(__file__).parents[0] / ".." / "."
+print(src)
+sys.path.append(str(src))
+
+from model import add, mul, predict
 
 def test_add():
     result = add(2, 2)
@@ -9,3 +23,7 @@ def test_add():
 def test_mul():
     result = mul(2, 2)
     assert result == 4
+
+
+if __name__ == "__main__":
+    main()
