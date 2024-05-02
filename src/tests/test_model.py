2f7416c511166256fe2e3f453f63095ef4fef222
LeSasse <l.sasse@fz-juelich.de>
2024-05-02 10:36:19 +0200

    clean up comment in test imports

--- src/tests/test_model.py
+++ src/tests/test_model.py
@@ -1,19 +1,20 @@
-
-
 import sys
 from pathlib import Path
 
-# We import is a bit messier and could simply
+# The import is a bit messier and could simply
 # be:
 # from ..model import add, mul, predict
 # if we only wanted to use pytest.
-# The below import enables us to use mut.py
-# as well as pytest:
+# The below import also enables us to directly
+# run tests by running python3 <name_of_file>
+# without packaging and installing our
+# project if we ever want to do that.
 src = Path(__file__).parents[0] / ".." / "."
 print(src)
 sys.path.append(str(src))
 
 from model import add, mul, predict
+
 
 def test_add():
     result = add(2, 2)
@@ -23,7 +24,3 @@ def test_add():
 def test_mul():
     result = mul(2, 2)
     assert result == 4
-
-
-if __name__ == "__main__":
-    main()
